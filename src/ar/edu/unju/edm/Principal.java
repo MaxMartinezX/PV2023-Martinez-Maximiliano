bc26368db4ce946196a4e54ebebbb48a42c3014b
maxip <maxip@192.168.0.10>
2023-04-02 13:23:00 -0300

    Maximiliano Martinez Primera Practica

---
+++ src/ar/edu/unju/edm/Principal.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		
+		System.out.println(Math.pow(a, 2));
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author maxip
+ *
+ */
+module Practica1 {
+}
